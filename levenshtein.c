6c0c3aa7bd031319f8d45effd1eb36c7901c29fc
Taguchi <tomodai.h.29@gmail.com>
2021-09-09 23:23:31 +0900

    optimize the program

--- levenshtein.c
+++ levenshtein.c
@@ -5,89 +5,67 @@
 int min(int, int, int);
 
 int main(int argc, char *argv[]) {
-    int n1, n2, n, i, j, cost;
+    int i, j, cost;
+    int n[3];
     int **score;
-    char *sentence1, *sentence2, *text;
-    FILE *fp1, *fp2;
+    char **sentence;
+    FILE *fp;
 
     if(argc < 3) {
         printf(" **Error: format: input 2 files after execution command.\n");
         exit(1);
     }
-    if((fp1=fopen(argv[1], "r")) == NULL) {
-        printf(" **Error: input file: cannot open the file \"%s\".\n", argv[1]);
-        exit(1);
-    }
-    if((fp2=fopen(argv[2], "r")) == NULL) {
-        printf(" **Error: input file: cannot open the file \"%s\".\n", argv[2]);
-        fclose(fp1);
-        exit(1);
-    }
-
-    n1 = n2 = 256;
-
-    n = n1 >> 1;
-    sentence1 = (char*) calloc(n1, sizeof(char));
-    text = (char*) calloc(n, sizeof(char));
-    sentence1[0] = ' ';
-    while(fgets(text, n, fp1) != NULL) {
-        if(strlen(sentence1) + strlen(text) > n1 - 1) {
-            n1 <<= 1; n <<= 1;
-            sentence1 = (char*) realloc(sentence1, sizeof(char) * n1);
-            text = (char*) realloc(text, sizeof(char) * n);
-            if(sentence1 == NULL || text == NULL) {
-                printf(" **Error: allocation: memory is insufficient.\n");
-                free(sentence1); free(text);
-                fclose(fp1); fclose(fp2);
-                exit(1);
-            }
+    
+    n[0] = n[1] = 256;
+    sentence = (char**) malloc(sizeof(char*) * 3);
+    for(i=0; i<2; i++) {
+        if((fp=fopen(argv[i+1], "r")) == NULL) {
+            printf(" **Error: input file: cannot open the file \"%s\".\n", argv[i+1]);
+            for(j=0; j<3; j++) free(sentence[j]);
+            free(sentence);
+            exit(1);
         }
-        strcat(sentence1, text);
-    }
-    n1 = strlen(sentence1) + 1;
-
-    n = n2 >> 1;
-    sentence2 = (char*) calloc(n2, sizeof(char));
-    text = (char*) calloc(n, sizeof(char));
-    sentence2[0] = ' ';
-    while(fgets(text, n, fp2) != NULL) {
-        if(strlen(sentence2) + strlen(text) > n2 - 1) {
-            n2 <<= 1; n <<= 1;
-            sentence2 = (char*) realloc(sentence2, sizeof(char) * n2);
-            text = (char*) realloc(text, sizeof(char) * n);
-            if(sentence2 == NULL || text == NULL) {
-                printf(" **Error: allocation: memory is insufficient.\n");
-                free(sentence1); free(sentence2); free(text);
-                fclose(fp1); fclose(fp2);
-                exit(1);
+        n[2] = n[i] >> 1;
+        sentence[i] = (char*) calloc(n[i], sizeof(char));
+        sentence[2] = (char*) calloc(n[2], sizeof(char));
+        sentence[i][0] = ' ';
+        while(fgets(sentence[2], n[2], fp) != NULL) {
+            if(strlen(sentence[i]) + strlen(sentence[2]) > n[i] - 1) {
+                n[i] <<= 1; n[2] <<= 1;
+                sentence[i] = (char*) realloc(sentence[i], sizeof(char) * n[i]);
+                sentence[2] = (char*) realloc(sentence[2], sizeof(char) * n[2]);
+                if(sentence[i] == NULL || sentence[2] == NULL) {
+                    printf(" **Error: allocation: memory is insufficient.\n");
+                    for(j=0; j<3; j++) free(sentence[j]);
+                    free(sentence);
+                    fclose(fp);
+                    exit(1);
+                }
             }
+            strcat(sentence[i], sentence[2]);
         }
-        strcat(sentence2, text);
+        n[i] = strlen(sentence[i]);
     }
-    n2 = strlen(sentence2) + 1;
 
-    fclose(fp1); fclose(fp2);
-
-    score = (int**) calloc(n1, sizeof(int*));
-    for(i=0; i<n1; i++) {
-        score[i] = (int*) calloc(n2, sizeof(int));
+    score = (int**) calloc(n[0], sizeof(int*));
+    for(i=0; i<n[0]; i++) {
+        score[i] = (int*) calloc(n[1], sizeof(int));
         score[i][0] = i;
     }
-    for(j=1; j<n2; j++) score[0][j] = j;
+    for(j=1; j<n[1]; j++) score[0][j] = j;
     
-    for(i=1; i<n1; i++) {
-        for(j=1; j<n2; j++) {
-            cost = sentence1[i]==sentence2[j] ? 0 : 1;
+    for(i=1; i<n[0]; i++) {
+        for(j=1; j<n[1]; j++) {
+            cost = sentence[0][i]==sentence[1][j] ? 0 : 1;
             score[i][j] = min(score[i-1][j] + 1, score[i][j-1] + 1, score[i-1][j-1] + cost);
         }
     }
 
-    printf("# of letters in \"%s\": %d\n", argv[1], n1 - 2);
-    printf("# of letters in \"%s\": %d\n", argv[2], n2 - 2);
-    printf("levenshtein distance: %d\n", score[n1-1][n2-1]);
+    for(i=0; i<2; i++) printf("# of letters in \"%s\": %d\n", argv[i+1], n[i] - 2);
+    printf("levenshtein distance: %d\n", score[n[0]-1][n[1]-1]);
 
-
-    free(sentence1); free(sentence2); free(text);
+    for(i=0; i<3; i++) free(sentence[i]);
+    free(sentence);
 
     return 0;
 }
